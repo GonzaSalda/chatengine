9153503719d50056b6b03eeb5af6df5a62eb02a5
GonzaSalda <gonza.salda00@gmail.com>
2024-07-29 16:55:20 -0300

    Add ChatFeed component and dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="5be14940-cd92-446a-a33e-efff5cc1172c"
+      userName="JsMastery"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
-}
-
+};
 export default App;
