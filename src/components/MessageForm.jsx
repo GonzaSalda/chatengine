a280de1b39477f1632a65ba37086069a0d5b60e3
GonzaSalda <gonza.salda00@gmail.com>
2024-07-30 19:45:06 -0300

    Add image upload functionality to MessageForm component

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,3 +1,4 @@
+import { PictureOutlined, SendOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import { sendMessage, isTyping } from "react-chat-engine";
 const MessageForm = (props) => {
@@ -12,6 +13,9 @@ const MessageForm = (props) => {
   const handleChange = (event) => {
     setValue(event.target.value);
   };
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: "" });
+  }
   return (
     <form className="message-form" onSubmit={handleSubmit}>
       <input
@@ -20,6 +24,21 @@ const MessageForm = (props) => {
         value={value}
         onChange={handleChange}
       />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon"/>
+      </button>
     </form>
   );
 };
